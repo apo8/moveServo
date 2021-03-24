691a7b9f8c172aa787eafb5a4aa7d5414ed13d6d
satoshi <hase.sato.8888@gmail.com>
2021-03-24 17:34:28 +0900

    start

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+	"periph.io/x/periph/conn/gpio"
+)
 
-}
+func main() {
+	adaptor := raspi.NewAdaptor()
+	servo := gpio.NewServoDriver(adaptor, "16")
+
+	offset := 13
+
+	work := func(){
+		servo.Move(uint8(27))
+	}
